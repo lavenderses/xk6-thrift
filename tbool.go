17603f1214c4affd74cd162c9b37c280f3a1bbc2
lavenderses <nakanoinryota@gmail.com>
2025-02-16 13:53:06 +0900

    Implement TRequest, generics Thrift request

--- tbool.go
+++ tbool.go
@@ -23,6 +23,22 @@ func (p *TBool) Equals(other *TValue) bool {
 	return p.value == o.value
 }
 
+func (p *TBool) WriteField(cxt context.Context, oprot thrift.TProtocol, fid int16, fname string) (err error) {
+	if err = oprot.WriteFieldBegin(cxt, fname, thrift.BOOL, fid); err != nil {
+		err = thrift.PrependError(fmt.Sprintf("%T write field begin error %d:%s: ", p, fid, fname), err)
+		return
+	}
+	if err = oprot.WriteBool(cxt, p.value); err != nil {
+		err = thrift.PrependError(fmt.Sprintf("%T.id (1) field write error: ", p), err)
+		return
+	}
+	if err = oprot.WriteFieldEnd(cxt); err != nil {
+		err = thrift.PrependError(fmt.Sprintf("%T write field end error %d:%s: ", p, fid, fname), err)
+		return
+	}
+	return
+}
+
 func (p *TBool) Read(ctx context.Context, iprot thrift.TProtocol) error {
 	if _, err := iprot.ReadStructBegin(ctx); err != nil {
 		return thrift.PrependError(fmt.Sprintf("%T read error: ", p), err)
--- tvalue.go
+++ tvalue.go
@@ -1,5 +1,14 @@
 package thrift
 
+import (
+	"context"
+
+	"github.com/apache/thrift/lib/go/thrift"
+)
+
 type TValue interface {
 	Equals(other *TValue) bool
+	// WriteField outputs TValue to Thrift protocol with field ID `fid` and field name `fname`.
+	// This must start with `WriteFieldBegin` method call, and end with `WriteFieldEnd` method call.
+	WriteField(cxt context.Context, oprot thrift.TProtocol, fid int16, fname string) error
 }
