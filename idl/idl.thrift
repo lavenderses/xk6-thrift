a470190bbffa42bff182d6601cfa9c4745eb5b7a
lavenderses <nakanoinryota@gmail.com>
2025-02-16 14:30:25 +0900

    Add new types structs and map

--- idl/idl.thrift
+++ idl/idl.thrift
@@ -1,7 +1,21 @@
 namespace java idl
 
+struct Nested {
+    1: string inner,
+}
+
+struct Message {
+    1: string content,
+    2: map<string, bool> tags,
+    3: Nested nested,
+}
+
 service TestService {
     string simpleCall(1: string id);
 
     bool boolCall(1: bool tf);
+
+    Message messageCall(1: Message message);
+
+    map<string, bool> mapCall(1: map<string, bool> maps);
 }
