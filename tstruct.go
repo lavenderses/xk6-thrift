8a06dbeee3fdc0e98fcf6eb4ade5dd156ac136ee
lavenderses <nakanoinryota@gmail.com>
2025-02-18 00:47:10 +0900

    comment

--- tstruct.go
+++ tstruct.go
@@ -46,6 +46,14 @@ func (p *TStruct) Equals(other *TValue) bool {
 	return true
 }
 
+// see [Thrift protocol spec @ 1a31d90 (v0.21.0)].
+//
+// [Thrift protocol spec @ 1a31d90 (v0.21.0)]: https://github.com/apache/thrift/blob/1a31d9051d35b732a5fce258955ef95f576694ba/doc/specs/thrift-protocol-spec.md (v0.21.0)
+// 
+// 	<field> ::= <field-begin> <field-data> <field-end>
+// 		<field-data> ::= I8 | I16 | I32 | I64 | DOUBLE | STRING | BINARY
+// 	<struct> | <map> | <list> | <set>
+// 	<struct> ::= <struct-begin> <field>* <field-stop> <struct-end>
 func (p *TStruct) WriteField(cxt context.Context, oprot thrift.TProtocol, fid int16, fname string) (err error) {
 	if err = oprot.WriteFieldBegin(cxt, fname, thrift.STRUCT, fid); err != nil {
 		err = thrift.PrependError(fmt.Sprintf("%T write field begin error %d:%s: ", p, fid, fname), err)
@@ -58,7 +66,7 @@ func (p *TStruct) WriteField(cxt context.Context, oprot thrift.TProtocol, fid in
 	// write struct fields recursively
 	for _, f := range slices.SortedFunc(maps.Keys(p.value), func(a, b TStructField) int {
 		return int(a.id) - int(b.id)
-	}){
+	}) {
 		p.value[f].WriteField(cxt, oprot, f.id, f.name)
 	}
 
