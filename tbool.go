cf6ae49ade89f7690f9a358499ba16a49fbd1aa5
cat <72122101+lavenderses@users.noreply.github.com>
2025-02-20 00:06:05 +0900

    Arrange method name and godoc comment to Thrift IDL spec (#17)
    * Add godoc comment for tstring
    
    * Add godoc comment for tbool
    
    * Add godoc comment for tmap
    
    g
    
    * Rename method to arrange spec
    
    * More comment

--- tbool.go
+++ tbool.go
@@ -23,6 +23,12 @@ func (p TBool) Equals(other *TValue) bool {
 	return p.value == o.value
 }
 
+// See [Thrift IDL protocol spec]
+//
+//   <field> ::= <field-begin> <field-data> <field-end>
+//   <field-data> ::= BOOL
+//
+// [Thrift IDL protocol spec]: https://github.com/apache/thrift/blob/eec0b584e657e4250e22f3fd492858d632e2aa7b/doc/specs/thrift-protocol-spec.md
 func (p TBool) WriteField(cxt context.Context, oprot thrift.TProtocol, fid int16, fname string) (err error) {
 	if err = oprot.WriteFieldBegin(cxt, fname, thrift.BOOL, fid); err != nil {
 		err = thrift.PrependError(fmt.Sprintf("%T write field begin error %d:%s: ", p, fid, fname), err)
--- tmap.go
+++ tmap.go
@@ -34,6 +34,14 @@ func (p *TMap) Equals(other *TValue) bool {
 	return true
 }
 
+// See [Thrift IDL protocol spec]
+//
+//   <field>      ::= <field-begin> <map> <field-end>
+//   <map>        ::= <map-begin> <field-data>* <map-end>
+//   <field-data> ::= I8 | I16 | I32 | I64 | DOUBLE | STRING | BINARY
+//                    <struct> | <map> | <list> | <set>
+//
+// [Thrift IDL protocol spec]: https://github.com/apache/thrift/blob/eec0b584e657e4250e22f3fd492858d632e2aa7b/doc/specs/thrift-protocol-spec.md
 func (p *TMap) WriteField(cxt context.Context, oprot thrift.TProtocol, fid int16, fname string) (err error) {
 	if err = oprot.WriteFieldBegin(cxt, fname, thrift.MAP, fid); err != nil {
 		err = thrift.PrependError(fmt.Sprintf("%T write field begin error %d:%s: ", p, fid, fname), err)
@@ -45,7 +53,7 @@ func (p *TMap) WriteField(cxt context.Context, oprot thrift.TProtocol, fid int16
 	}
 
 	for k, v := range p.value {
-		if err = p.writePair(cxt, oprot, k, v); err != nil {
+		if err = p.writeFieldDataKeyValue(cxt, oprot, k, v); err != nil {
 			err = thrift.PrependError(fmt.Sprintf("%T write key value (field %d) error", p, fid), err)
 			return
 		}
@@ -63,17 +71,26 @@ func (p *TMap) WriteField(cxt context.Context, oprot thrift.TProtocol, fid int16
 	return
 }
 
-func (p *TMap) writePair(cxt context.Context, oprot thrift.TProtocol, k, v TValue) (err error) {
-	if err = p.writeEntry(cxt, oprot, k); err != nil {
+// See the above spec.
+//
+//   <map>        ::= <map-begin> <field-data>* <map-end>
+//   <field-data> ::= I8 | I16 | I32 | I64 | DOUBLE | STRING | BINARY
+//                    <struct> | <map> | <list> | <set>
+func (p *TMap) writeFieldDataKeyValue(cxt context.Context, oprot thrift.TProtocol, k, v TValue) (err error) {
+	if err = p.writeFieldData(cxt, oprot, k); err != nil {
 		return
 	}
-	if err = p.writeEntry(cxt, oprot, v); err != nil {
+	if err = p.writeFieldData(cxt, oprot, v); err != nil {
 		return
 	}
 	return
 }
 
-func (p *TMap) writeEntry(cxt context.Context, oprot thrift.TProtocol, value TValue) (err error) {
+// See the above spec.
+//
+//   <field-data> ::= I8 | I16 | I32 | I64 | DOUBLE | STRING | BINARY
+//                    <struct> | <map> | <list> | <set>
+func (p *TMap) writeFieldData(cxt context.Context, oprot thrift.TProtocol, value TValue) (err error) {
 	if o, ok := value.(TString); ok {
 		oprot.WriteString(cxt, o.value)
 	} else if o, ok := value.(TBool); ok {
