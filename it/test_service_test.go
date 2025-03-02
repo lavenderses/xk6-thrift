dbda0bd6528ec536ca17357e299e8a7528767b3b
lavenderses <nakanoinryota@gmail.com>
2025-03-02 22:28:47 +0900

    Implement Thrift type list

--- it/test_service_test.go
+++ it/test_service_test.go
@@ -204,3 +204,38 @@ func TestMessageCall(t *testing.T) {
 
 	assertEquals(t, *actual, *expect)
 }
+
+func TestStringCall(t *testing.T) {
+	// prepare
+	var client *thrift.TStandardClient
+	var err error
+	if client, err = setupClient(t); err != nil {
+		t.Fatalf("error creating client. %v", err)
+	}
+
+	cxt := context.Background()
+	method := "stringCall"
+	value := []xk6_thrift.TValue{
+		xk6_thrift.NewTstring("content-1"),
+		xk6_thrift.NewTstring("content-2"),
+	}
+	tvalue := map[int16]xk6_thrift.TValue{
+		1: xk6_thrift.NewTList(&value, thrift.STRING),
+	}
+	arg := xk6_thrift.NewTRequestWithValue(&tvalue)
+	expectValue := []xk6_thrift.TValue{
+		xk6_thrift.NewTstring("content-1:content-1"),
+		xk6_thrift.NewTstring("content-2:content-2"),
+	}
+	expectTValue := xk6_thrift.NewTList(&expectValue, thrift.STRING)
+	expect := xk6_thrift.NewTResponse()
+	expect.Add(0, expectTValue)
+	actual := xk6_thrift.NewTResponse()
+
+	// do & verify
+	if _, err = (*client).Call(cxt, method, arg, actual); err != nil {
+		t.Fatalf("error calling RPC. %v", err)
+	}
+
+	assertEquals(t, *actual, *expect)
+}
