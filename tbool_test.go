668f4841c21020cf840b31ffc195f30478239726
lavenderses <nakanoinryota@gmail.com>
2025-03-09 21:57:48 +0900

    Add unit test for boolean

---
+++ tbool_test.go
@@ -0,0 +1,116 @@
+package thrift
+
+import (
+	"context"
+	"testing"
+)
+
+func TestEquals_TBool_equals(t *testing.T) {
+	// prepare
+	a := NewTBool(true)
+	var b TValue
+	b = NewTBool(true)
+	expected := true
+
+	// do
+	actual := a.Equals(&b)
+
+	// verify
+	assert(t, "", actual, expected)
+}
+
+func TestEquals_TBool_not_equals(t *testing.T) {
+	// prepare
+	a := NewTBool(true)
+	var b TValue
+	b = NewTBool(false)
+	expected := false
+
+	// do
+	actual := a.Equals(&b)
+
+	// verify
+	assert(t, "", actual, expected)
+}
+
+func TestEquals_TBool_other_type(t *testing.T) {
+	// prepare
+	a := NewTBool(true)
+	var b TValue
+	b = NewTstring("other")
+	expected := false
+
+	// do
+	actual := a.Equals(&b)
+
+	// verify
+	assert(t, "", actual, expected)
+}
+
+func TestWriteFieldData(t *testing.T) {
+	// prepare
+	oprot := setupProtocol(t)
+	cxt := context.Background()
+	value := NewTBool(true)
+	expected := true
+
+	// do
+	err := value.WriteFieldData(cxt, oprot)
+	checkError(t, err)
+
+	// verify
+	oprot.Flush(cxt)
+	actual, err := oprot.ReadBool(cxt)
+	checkError(t, err)
+	assert(t, "", actual, expected)
+}
+
+func TestReadBool_true(t *testing.T) {
+	// prepare
+	iprot := setupProtocol(t)
+	cxt := context.Background()
+	iprot.WriteBool(cxt, true)
+	iprot.Flush(cxt)
+
+	// do
+	actual, err := ReadBool(cxt, iprot)
+	checkError(t, err)
+
+	// verfiy
+	e := NewTBool(true)
+	a, ok := actual.(TBool)
+	assertTrue(t, "cast to TBool", ok)
+	assert(t, "result", a.value, e.value)
+}
+
+func TestReadBool_false(t *testing.T) {
+	// prepare
+	iprot := setupProtocol(t)
+	cxt := context.Background()
+	iprot.WriteBool(cxt, false)
+	iprot.Flush(cxt)
+
+	// do
+	actual, err := ReadBool(cxt, iprot)
+	checkError(t, err)
+
+	// verfiy
+	e := NewTBool(false)
+	a, ok := actual.(TBool)
+	assertTrue(t, "cast to TBool", ok)
+	assert(t, "result", a.value, e.value)
+}
+
+func TestReadBool_invalid_type(t *testing.T) {
+	// prepare
+	iprot := setupProtocol(t)
+	cxt := context.Background()
+	iprot.WriteString(cxt, "FOO")
+	iprot.Flush(cxt)
+
+	// do
+	_, err := ReadBool(cxt, iprot)
+
+	// verfiy
+	assertTrue(t, "error expected", err != nil)
+}
